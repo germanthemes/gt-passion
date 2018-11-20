2ec0a63c0d9fc9d3882cd5fbf87c0ad4ea350808
Thomas Weichselbaumer <kontakt@themecoder.de>
2018-11-20 11:03:20 +0100

    added custom background feature

--- functions.php
+++ functions.php
@@ -58,6 +58,11 @@ function gt_workout_setup() {
 		'height'      => 640,
 	) ) );
 
+	// Set up the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'gt_workout_custom_background_args', array(
+		'default-color' => 'ffffff',
+	) ) );
+
 	// Add Theme Support for Selective Refresh in Customizer.
 	add_theme_support( 'customize-selective-refresh-widgets' );
 }
