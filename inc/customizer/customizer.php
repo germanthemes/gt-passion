c3153e3d3174c25a0cb303cc7961618ccee69547
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-08-25 15:07:32 +0200

    fixed font control javascript issues in Customizer

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -66,7 +66,7 @@ add_action( 'customize_preview_init', 'gt_passion_customize_preview_js' );
  * Embed JS for Customizer Controls.
  */
 function gt_passion_customizer_controls_js() {
-	wp_enqueue_script( 'gt-passion-customizer-controls', get_template_directory_uri() . '/assets/js/customizer-controls.min.js', array(), '20191130', true );
+	wp_enqueue_script( 'gt-passion-customizer-controls', get_template_directory_uri() . '/assets/js/customizer-controls.min.js', array(), '20200825', true );
 }
 add_action( 'customize_controls_enqueue_scripts', 'gt_passion_customizer_controls_js' );
 
