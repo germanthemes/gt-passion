39f110e83de5b85e6127ffb18d0e9654eeb6bf14
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-09-08 10:26:31 +0200

    updated minimum required WordPress Core version

--- functions.php
+++ functions.php
@@ -6,6 +6,15 @@
  */
 
 /**
+ * GT Passion only works in WordPress 5.3 or later.
+ */
+if ( version_compare( $GLOBALS['wp_version'], '5.3', '<' ) ) {
+	require get_template_directory() . '/inc/admin/back-compat.php';
+	return;
+}
+
+
+/**
  * Sets up theme defaults and registers support for various WordPress features.
  *
  * Note that this function is hooked into the after_setup_theme hook, which
@@ -208,7 +217,7 @@ function gt_passion_theme_updater() {
 		$theme_updater = new GT_Passion_Theme_Updater(
 			array(
 				'remote_api_url' => GT_PASSION_STORE_API_URL,
-				'version'        => '1.5',
+				'version'        => '1.5.1',
 				'license'        => trim( gt_passion_get_option( 'license_key' ) ),
 				'item_id'        => GT_PASSION_PRODUCT_ID,
 				'item_name'      => 'GT Passion',
--- inc/admin/back-compat.php
+++ inc/admin/back-compat.php
@@ -2,9 +2,9 @@
 /**
  * GT Passion back compat functionality
  *
- * Prevents GT Passion from running on WordPress versions prior to 4.7,
+ * Prevents GT Passion from running on WordPress versions prior to 5.3,
  * since this theme is not meant to be backward compatible beyond that and
- * relies on many newer functions and markup changes introduced in 4.7.
+ * relies on many newer functions and markup changes introduced in 5.3.
  *
  * @package GT Passion
  *
@@ -34,35 +34,35 @@ add_action( 'after_switch_theme', 'gt_passion_switch_theme' );
  * Adds a message for unsuccessful theme switch.
  *
  * Prints an update nag after an unsuccessful attempt to switch to
- * GT Passion on WordPress versions prior to 4.7.
+ * GT Passion on WordPress versions prior to 5.3.
  *
  * @global string $wp_version WordPress version.
  */
 function gt_passion_upgrade_notice() {
-	$message = sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'gt-passion' ), 'GT Passion', '4.7', $GLOBALS['wp_version'] );
+	$message = sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'gt-passion' ), 'GT Passion', '5.3', $GLOBALS['wp_version'] );
 	printf( '<div class="error"><p>%s</p></div>', $message );
 }
 
 /**
- * Prevents the Customizer from being loaded on WordPress versions prior to 4.7.
+ * Prevents the Customizer from being loaded on WordPress versions prior to 5.3.
  *
  * @global string $wp_version WordPress version.
  */
 function gt_passion_customize() {
-	wp_die( sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'gt-passion' ), 'GT Passion', '4.7', $GLOBALS['wp_version'] ), '', array(
+	wp_die( sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'gt-passion' ), 'GT Passion', '5.3', $GLOBALS['wp_version'] ), '', array(
 		'back_link' => true,
 	) );
 }
 add_action( 'load-customize.php', 'gt_passion_customize' );
 
 /**
- * Prevents the Theme Preview from being loaded on WordPress versions prior to 4.7.
+ * Prevents the Theme Preview from being loaded on WordPress versions prior to 5.3.
  *
  * @global string $wp_version WordPress version.
  */
 function gt_passion_preview() {
 	if ( isset( $_GET['preview'] ) ) {
-		wp_die( sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'gt-passion' ), 'GT Passion', '4.7', $GLOBALS['wp_version'] ) );
+		wp_die( sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'gt-passion' ), 'GT Passion', '5.3', $GLOBALS['wp_version'] ) );
 	}
 }
 add_action( 'template_redirect', 'gt_passion_preview' );
